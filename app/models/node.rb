2273a39ec27daa03d8a5a1822b89d6c6ee6084eb
Joshua Shane Martin <josmar52789@gmail.com>
2014-01-22 18:54:12 -0500

    Printing inventory

--- app/models/node.rb
+++ app/models/node.rb
@@ -16,10 +16,11 @@ class Node < ActiveRecord::Base
   validates :role, :inclusion => {:in => ["Workstation", "Server"]}
 
   def self.create_or_update_from_inventory(inventory)
-    node = find_or_create_by(uuid: inventory[:uuid])
+    logger.info inventory
+    node = find_or_create_by(uuid: inventory['uuid'])
     node.update(inventory.except(:uuid))
 
-    inventory[:applications].each do |a|
+    inventory['applications'].each do |a|
       application = Application.find_or_create_by( name: a[:name],
                                                    publisher: a[:publisher],
                                                    version: a[:version] )
