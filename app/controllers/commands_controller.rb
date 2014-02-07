e5380bb52a168587e7ed8b9840a544126a67df10
Joshua Shane Martin <josmar52789@gmail.com>
2014-02-07 10:28:43 -0500

    Cleaning up commands controller.

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -1,27 +1,12 @@
 class CommandsController < WebsocketRails::BaseController
 
-  # Creates or updates the database with the given inventory payload.
-  # The data received from the Liberatio client should resemble:
   #
   # {
-  #   uuid: '05a27fe0-7f25-11e3-b9de-0002a5d5c51b',
-  #   role: 'Server',
-  #   location: 'My Office',
-  #   name: 'station-01',
-  #   operating_system: 'Windows XP Professional SP3',
-  #   serial_number: 'ABC123',
-  #   model_number: 'MacBook Pro Early 2011',
-  #   applications: { 
-  #                   application: {
-  #                                  name: '7-zip',
-  #                                  publisher: '7-zip',
-  #                                  version: '4.65'
-  #                                }
-  #                 }
+  #   uuid: '05a27fe0-7f25-11e3-b9de-0002a5d5c51b'
   # }
   def create
-    inventory = Node.create_or_update_from_inventory(message)
-    send_message :create_success, "Success", :namespace => :inventories
+    # inventory = Node.create_or_update_from_inventory(message)
+    send_message :create_success, "Success", :namespace => :commands
 
     # if task.save
     #   send_message :create_success, task, :namespace => :inventories
