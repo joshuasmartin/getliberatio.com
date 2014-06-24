551fde003f8edad40a2da8b1fcec7e792b2a8eae
Joshua Shane Martin <josmar52789@gmail.com>
2014-06-23 21:20:15 -0400

    Attempt find method

--- app/models/node.rb
+++ app/models/node.rb
@@ -129,7 +129,7 @@ class Node < ActiveRecord::Base
 
   def online?
     response = Pusher.get("/channels/presence-cmd_#{self.uuid}/users")
-    response.any? { |u| u[:id] == self.id }
+    if (response.find { |u| u["id"] == self.id }).present?
   end
 
 
