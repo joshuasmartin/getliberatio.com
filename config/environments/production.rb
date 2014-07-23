2f06d5c986ec658f0908e15f9847a26062464634
Joshua Shane Martin <josmar52789@gmail.com>
2014-07-23 14:51:51 -0400

    Adding css compressor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,7 @@ Liberatio::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(mangle: false)
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
