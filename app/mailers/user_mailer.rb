0854158c9674029f5af57f689b8d0374cb4ee75f
Joshua Shane Martin <josmar52789@gmail.com>
2014-07-23 12:22:15 -0400

    Using layout for Email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,9 @@
 class UserMailer < ActionMailer::Base
   default from: "noreply@getliberatio.com"
 
+  # Use views/layouts/user_mailer.(html.haml|.text.erb)
+  layout 'user_mailer'
+
   def welcome_email(user)
     @user = user
     mail(to: @user.email_address, subject: "Welcome to Liberatio")
